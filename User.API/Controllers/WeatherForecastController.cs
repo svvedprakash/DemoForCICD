3ab1af80f52437eff871b98a882ed36bc8533c9c
svvedprakash <sv.vedprakash@gmail.com>
2021-02-20 19:23:30 +0530

    My Test 01

--- User.API/Controllers/WeatherForecastController.cs
+++ User.API/Controllers/WeatherForecastController.cs
@@ -11,6 +11,7 @@ namespace User.API.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
